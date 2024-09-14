6c987096a9dab60c410faa79328f2daceb3dc13b
Mohsan Raza <mohsan.raza@paceglobalpk.net>
2024-09-14 15:18:32 -0700

    edit-add-and-get-data-with-file

--- index.js
+++ index.js
@@ -9,11 +9,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", 'ejs')
 
 app.get("/", function(req , res){
-    // if i set file loaded on route then use render keyword
     fs.readdir(`./files`, function(err, files){
-        console.log(files)
+        res.render("index", {files: files})
     })
-    res.render("index")
+})
+app.post("/create", function(req , res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/")
+    });
+})
+app.get("/file/:filename", function(req, res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render("show", {filename: req.params.filename, filedata: filedata})
+    })
+})
+app.get("/edit/:filename", function(req, res){
+    // fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+    res.render("edit", {filename: req.params.filename})
+    // })
+})
+app.post("/edit", function(req, res){
+    fs.rename(`./files/${req.body.prev_title}`, `./files/${req.body.new_title}`, function(err){
+        res.redirect("/")
+    });
 })
 app.get("/profile/:username/:age", function(req, res){
     res.send(`Name: ${req.params.username} Age: ${req.params.age}`)
